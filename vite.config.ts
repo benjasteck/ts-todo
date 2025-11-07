88bcb075d850042a70b371af037dd8ddf1bacefe
benjasteck <benj5102@easv365.dk>
2025-11-07 12:15:56 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/todo/',
+  server: {
+    host: true, // binds to 0.0.0.0
+    port: 5173
+  },
+  base: '/todo/'
 })
